1c1ce0b4e21eda1d74bb18776f37a9f2c3a658b8
leebock <lbock@esri.com>
2023-05-23 10:57:25 -0400

    installed the hash / section scaffolding and a bit of css.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import './App.css';
 
+import { useState, useEffect } from 'react';
+
 function App() {
+
+  const [section, setSection] = useState(0);  
+
+  useEffect(
+    ()=>{
+      window.addEventListener(
+        "hashchange", 
+        e => setSection(
+          parseInt(window.location.hash.replace("#","").trim()) || 0
+        )
+      );      
+    },
+    []
+  );
+
+
+  useEffect(
+    ()=>{
+      switch(section) {
+        default:
+      }
+    },
+    [section]
+  )
+
+
   return (
     <div className="App">
+      <div>Section: <strong>{section}</strong></div>
     </div>
   );
 }
