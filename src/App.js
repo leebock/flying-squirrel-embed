f7a874b1b3c1e2342d507526670fab7bb96fb18a
leebock <lbock@esri.com>
2023-05-23 16:50:00 -0400

    adding route and moving the camera!

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import { SquirrelMap } from './components/SquirrelMap';
 
 function App() {
 
-  const [section, setSection] = useState(0);  
+  const [chapter, setChapter] = useState(0);  
+  const [camera, setCamera] = useState(null);
 
   useEffect(
     ()=>{
       window.addEventListener(
         "hashchange", 
-        e => setSection(
+        e => setChapter(
           parseInt(window.location.hash.replace("#","").trim()) || 0
         )
       );      
@@ -22,17 +23,71 @@ function App() {
 
   useEffect(
     ()=>{
-      switch(section) {
+
+      switch(chapter) {
+        case 1:
+          setCamera(
+            {
+              position: {
+                x: -82.53799843356778,
+                y: 35.62626185903141,
+                z: 1094.2781136026606
+              },
+              tilt: 72.90990972588057,
+              heading: 216.55365390783064,
+              fov: 100
+            }            
+          );
+          break;
+        case 2:
+          setCamera(
+            {
+              position: {
+                x: -82.54764699782481,
+                y: 35.62631501857084 ,
+                z: 1173.9075362496078
+              },
+              tilt: 65.34093408568543,
+              heading: 192.74796920864986
+            }
+          );
+          break;
+        case 3:
+          setCamera(
+            {
+              position: {
+                x: -82.55403802256195,
+                y: 35.6158699141095 ,
+                z: 971.4670652030036
+              },
+              tilt: 55.36133679947065,
+              heading: 145.85071494035003
+            }
+          );
+          break;
+        case 4:
+          setCamera(
+            {
+              position: {
+                x: -82.54604312292328 ,
+                y: 35.60849254268956 ,
+                z: 760.1628188071772
+              },
+              tilt: 84.82919450156275,
+              heading: 184.0505151777685
+            }
+          );
+          break;  
         default:
       }
     },
-    [section]
+    [chapter]
   )
 
 
   return (
     <div className="App">
-      <SquirrelMap></SquirrelMap>
+      <SquirrelMap camera={camera}></SquirrelMap>
     </div>
   );
 }
--- src/components/SquirrelMap.js
+++ src/components/SquirrelMap.js
@@ -1,33 +1,89 @@
 import { useRef, useEffect } from "react";
 import Map from "@arcgis/core/Map.js";
 import SceneView from "@arcgis/core/views/SceneView.js";
+import GeoJSONLayer from "@arcgis/core/layers/GeoJSONLayer";
+import _geoJsonRoute from "../data/saturday-run.json";
 import '@arcgis/core/assets/esri/css/main.css';
 
-export const SquirrelMap = ({})=>{
+export const SquirrelMap = ({camera})=>{
     const _refMap = useRef(null);
     const _refView = useRef(null);
     useEffect(
         ()=>{
-            _refMap.current = new Map(
-                {basemap: "satellite", ground: "world-elevation"}
-            );
+          _refMap.current = new Map(
+              {basemap: "satellite", ground: "world-elevation"}
+          );
+
+          const blob = new Blob(
+            [JSON.stringify(_geoJsonRoute)], 
+            {type: "application/json"}
+          );
+          // URL reference to the blob
+          const routeLayer = new GeoJSONLayer(
+            {
+              url: URL.createObjectURL(blob),
+              elevationInfo: "relative-to-ground",
+              renderer: {
+                type: "simple",
+                symbol: {
+                  type: "line-3d",  // autocasts as new LineSymbol3D()
+                  symbolLayers: [{
+                    type: "path",  // autocasts as new PathSymbol3DLayer()
+                    profile: "circle",  // creates a rectangular shape
+                    width: 10,  // path width will also set the height to the same value
+                    material: { color: "#ff7380" },
+                    cap: "round"
+                  }]
+                }
+              }
+            }
+          );
+
+          _refMap.current.add(routeLayer);
 	        _refView.current = new SceneView({
                 container: "view",
-                map: _refMap.current/*,
+                map: _refMap.current,
                 camera: {
                   position: {
-                    x: ANIMATIONS[0].result.x,
-                    y: ANIMATIONS[0].result.y,
-                    z: ANIMATIONS[0].result.z
+                    x: -82.57080137652181,
+                    y: 35.61561421226409,
+                    z: 1931
                   },
-                  tilt: ANIMATIONS[0].result.tilt,
-                  heading: ANIMATIONS[0].result.heading,
-                  fov: ANIMATIONS[0].result.fov
-                }*/
+                  tilt: 67,
+                  heading: 93,
+                  fov: 100
+                }
               });
-  
+
+          
+          _refView.current.watch(
+            "camera",
+            (camera) => {
+              const {tilt, heading, fov} = camera;
+              const {latitude, longitude, z} = camera.position;
+              console.log("******************************");
+              console.log("position", latitude, longitude, z)
+              console.log("tilt", tilt);
+              console.log("heading", heading);
+              console.log("fov", fov);
+              console.log(JSON.stringify(camera.toJSON()))
+            }
+          );
+          
+
         },
         []
     );
+
+    useEffect(
+      ()=>{
+        _refView.current.goTo(
+          camera,
+          {animate: true, duration: 3000, easing: "in-out-cubic"}
+        );
+      },
+      [camera]
+    );
+
     return <div id="view"></div>
 }
