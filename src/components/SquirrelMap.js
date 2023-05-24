d68e5f25837e666a2185fec89ecfccef1751b907
leebock <lbock@esri.com>
2023-05-24 14:08:47 -0400

    added some labels

--- src/components/SquirrelMap.js
+++ src/components/SquirrelMap.js
@@ -2,12 +2,16 @@ import { useRef, useEffect } from "react";
 import Map from "@arcgis/core/Map.js";
 import SceneView from "@arcgis/core/views/SceneView.js";
 import GeoJSONLayer from "@arcgis/core/layers/GeoJSONLayer";
+import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer";
+import Graphic from "@arcgis/core/Graphic";
 import _geoJsonRoute from "../data/saturday-run.json";
 import '@arcgis/core/assets/esri/css/main.css';
 
 export const SquirrelMap = ({camera})=>{
     const _refMap = useRef(null);
     const _refView = useRef(null);
+    const _refGraphicsLayer = useRef(null);
+    
     useEffect(
         ()=>{
           _refMap.current = new Map(
@@ -55,7 +59,46 @@ export const SquirrelMap = ({camera})=>{
                 }
               });
 
-          
+
+          _refGraphicsLayer.current = new GraphicsLayer({elevationInfo: "relative-to-ground"});
+          _refMap.current.add(_refGraphicsLayer.current)
+
+          const ts3dl = {
+            type: "text",  // autocasts as new TextSymbol3DLayer()
+            material: { color: [ 255,255,0 ] },
+            background: { color: [0, 0, 0, 0.75] },
+            size: 11  // points
+          }
+  
+          _refGraphicsLayer.current.addMany(
+            [
+              new Graphic(
+                {
+                  geometry: {type: "point",x: -82.54234061560147,y: 35.62110855418713,z: 200}, 
+                  symbol: {type: "point-3d", symbolLayers: [{...ts3dl, text: "Grove Park Inn"}]}
+                }
+              ),
+              new Graphic(
+                {
+                  geometry: {type: "point",x: -82.54681495295014,y: 35.60458161641058, z: 200}, 
+                  symbol: {type: "point-3d", symbolLayers: [{...ts3dl, text: "Taco Temple"}]}
+                }
+              ),
+              new Graphic(
+                {
+                  geometry: {type: "point", x: -82.5539519233415, y: 35.61412957325879}, 
+                  symbol: {type: "point-3d", symbolLayers: [{...ts3dl, text: "Weaver Park"}]}
+                }
+              ),
+              new Graphic(
+                {
+                  geometry: {type: "point", x: -82.53178870691916, y: 35.623872249757895},
+                  symbol: {type: "point-3d", symbolLayers: [{...ts3dl, text: "Western Residence"}]}
+                }
+              )
+            ]
+          );
+
           _refView.current.watch(
             "camera",
             (camera) => {
