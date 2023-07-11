05f0e39039993fbd32f5e93f69854699505fd371
leebock <lbock@esri.com>
2023-07-11 16:59:55 -0400

    add navigation UI when app is not embedded

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 import { useState, useEffect } from 'react';
@@ -8,6 +9,7 @@ function App() {
   const [slide, setSlide] = useState(0);  
   const [camera, setCamera] = useState(null);
   const [stage, setStage] = useState(null);
+  const [showUI, setShowUI] = useState(true);
 
   useEffect(
     ()=>{
@@ -17,6 +19,10 @@ function App() {
           parseInt(window.location.hash.replace("#","").trim()) || 0
         )
       );      
+      // hide UI if this is an embed
+      if (window.location !== window.parent.location) {
+        setShowUI(false);
+      }
     },
     []
   );
@@ -82,12 +88,39 @@ function App() {
     [slide]
   )
 
+  const populatePagination = () => 
+    Array
+    .from(Array(14).keys())
+    .map(
+      (value)=>(
+        <li className="page-item" key={value}>
+          <button className={"page-link"+(value===stage ? " active" : "")} 
+                  type="button"
+                  onClick={()=>{setSlide(value)}}>
+                    {value === 0 ? "Intro" : value}
+          </button>
+        </li>
+      )
+    )
+  
 
   return (
     <div className="App">
       <SquirrelMap camera={camera} stage={stage}></SquirrelMap>
+      {
+      showUI &&
+      <nav className="position-absolute d-none d-sm-block" style={{"bottom": "20px"}} aria-label="Slide controls">
+        <ul className="pagination pagination-lg d-none d-lg-flex">
+          {populatePagination()}
+        </ul>
+        <ul className="pagination d-none d-sm-flex d-lg-none">
+          {populatePagination()}
+        </ul>
+      </nav>
+      }
     </div>
   );
+
 }
 
 export default App;
